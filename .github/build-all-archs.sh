5c146f32a9f71eab38f8ad3eeacdba17e49d5a14
Rikpat <kadlcik.patrik@gmail.com>
2025-03-03 23:05:17 +0100

    ci: changed package name

--- .github/build-all-archs.sh
+++ .github/build-all-archs.sh
@@ -9,7 +9,7 @@ for platform in $BUILD_ARCHS; do
         output_name+='.exe'
     fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o $output_name github.com/Rikpat/tfmigrate
+    env GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=0 go build -o $output_name github.com/minamijoyo/tfmigrate
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
